53d751e2330c0b8c10041ce6c59d5d90eba7b8ba
Filipe Silva <filipematossilva@gmail.com>
2023-11-05 23:30:01 +0000

    test db

--- src/fdb/db.clj
+++ src/fdb/db.clj
@@ -1,46 +1,36 @@
 (ns fdb.db
-  (:refer-clojure :exclude [get])
+  (:refer-clojure :exclude [get sync])
   (:require
-   [clojure.java.io :as io]
+   [babashka.fs :as bb-fs]
    [xtdb.api :as xt]))
 
-(defonce state (atom nil))
-
-(defn start!
-  [path]
+(defn start
+  [db-path]
   (let [cfg {:xtdb/index-store         {:kv-store {:xtdb/module 'xtdb.rocksdb/->kv-store
-                                                   :db-dir      (io/file path "rocksdb/index")}}
+                                                   :db-dir      (bb-fs/file db-path "rocksdb/index")}}
              :xtdb/document-store      {:kv-store {:xtdb/module 'xtdb.rocksdb/->kv-store
-                                                   :db-dir      (io/file path "rocksdb/document")}}
+                                                   :db-dir      (bb-fs/file db-path "rocksdb/document")}}
              :xtdb/tx-log              {:kv-store {:xtdb/module 'xtdb.rocksdb/->kv-store
-                                                   :db-dir      (io/file path "rocksdb/tx-log")}}
-             :xtdb.lucene/lucene-store {:db-dir (io/file path "lucene")}}]
-    (swap! state (fn [node]
-                  (when node (.close node))
-                  (xt/start-node cfg)))))
+                                                   :db-dir      (bb-fs/file db-path "rocksdb/tx-log")}}
+             :xtdb.lucene/lucene-store {:db-dir (bb-fs/file db-path "lucene")}}]
+    (xt/start-node cfg)))
 
-(defn stop!
-  []
-  (swap! state (fn [node]
-                 (when node (.close node))
-                 nil)))
+(defn sync
+  [node]
+  (xt/sync node))
 
 (defn put
-  [data]
-  {:pre [(some? @state)]}
-  (xt/submit-tx @state [[::xt/put data]]))
+  [node id data]
+  (xt/submit-tx node [[::xt/put (merge {:xt/id id} data)]]))
 
 (defn delete
-  [id]
-  {:pre [(some? @state)]}
-  (xt/submit-tx @state [[:xt/delete id]]))
+  [node id]
+  (xt/submit-tx node [[:xt/delete id]]))
 
 (defn get
-  [id]
-  {:pre [(some? @state)]}
-  (xt/entity (xt/db @state) id))
+  [node id]
+  (xt/entity (xt/db node) id))
 
 (defn query
-  [query & args]
-  {:pre [(some? @state)]}
-  (apply xt/q (xt/db @state) query args))
+  [node query & args]
+  (apply xt/q (xt/db node) query args))
---
+++ test/fdb/db_test.clj
@@ -0,0 +1,13 @@
+(ns fdb.db-test
+  (:require
+   [fdb.db :as sut]
+   [babashka.fs :refer [with-temp-dir]]
+   [clojure.test :refer [deftest is]]))
+
+(deftest make-me-a-db
+  (with-temp-dir [db-path {}]
+    (with-open [node (sut/start db-path)]
+      (sut/put node :foo {:bar "bar"})
+      (sut/sync node)
+      (is (= {:xt/id :foo :bar "bar"}
+             (sut/get node :foo))))))
