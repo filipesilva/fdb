08d06fbcbd1c2ed867bc54934a3cbef7d6c3852c
Filipe Silva <filipematossilva@gmail.com>
2023-11-21 23:41:04 +0000

    calls

--- src/fdb/call.clj
+++ src/fdb/call.clj
@@ -1,6 +1,39 @@
-(ns fdb.call)
+(ns fdb.call
+  (:require
+   [babashka.process :refer [shell]]
+   [taoensso.timbre :as log]))
 
-(defn to-fn
+(defmulti to-fn
+  "Takes call-spec and returns a function that takes a call-arg.
+  call-specs are dispatched by type:
+  - symbol: Resolves and returns the var
+  - list:   Evaluates and returns the result
+  - vec:    Runs shell command via babashka.process/shell
+            You can use the shell option map, and the config-path,
+            doc-path and self-path bindings."
+  (fn [call-spec] (type call-spec)))
+
+(defmethod to-fn :default
   [call-spec]
+  (fn [_call-arg]
+    (log/error "Unknown call-spec" call-spec)))
+
+(defmethod to-fn clojure.lang.Symbol
+  [sym]
+  (resolve sym))
+
+(defmethod to-fn clojure.lang.PersistentList
+  [sexp]
+  (eval sexp))
+
+(defmethod to-fn clojure.lang.PersistentVector
+  [shell-args]
   (fn [call-arg]
-    call-arg))
+    (let [bind-args     (eval (list 'fn '[{:keys [config-path doc-path self-path]}] shell-args))
+          shell-args'   (bind-args call-arg)
+          [opts & rest] shell-args'
+          io-opts       {:out *out* :err *err*}
+          shell-args''  (if (map? opts)
+                          (into [(merge io-opts opts)] rest)
+                          (into [io-opts] shell-args'))]
+      (apply shell shell-args''))))
---
+++ test/fdb/call_test.clj
@@ -0,0 +1,20 @@
+(ns fdb.call-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [fdb.call :as sut]))
+
+(deftest symbol-test
+  (is (= 43 ((sut/to-fn 'inc) 42))))
+
+(deftest sexp-test
+  (is (= 43 ((sut/to-fn '(fn [x] (inc x))) 42)))
+  (is (= "foo" ((sut/to-fn
+                 '(fn [x]
+                    (clojure.string/lower-case x)))
+                "FOO"))))
+
+(deftest shell-text
+  (is (= "1 2 3\n"
+         (with-out-str
+           ((sut/to-fn '["echo" config-path doc-path self-path])
+            {:config-path "1" :doc-path "2" :self-path "3"})))))
