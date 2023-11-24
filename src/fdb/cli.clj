5a5a5db68ce5860d0045ee408ddcb8731599087b
Filipe Silva <filipematossilva@gmail.com>
2023-11-24 00:15:36 +0000

    really make the fdb bin work with bbin, anywhere

--- src/fdb/cli.clj
+++ src/fdb/cli.clj
@@ -1,28 +1,35 @@
 (ns fdb.cli
   (:refer-clojure :exclude [sync])
   (:require
-   [fdb.core :as fdb]
    [babashka.cli :as cli]))
 
 (defn watch [m]
-  (assoc m :fn :watch))
+  ;; only load everything when we need it, so we can have fast call and sync
+  (let [watch (requiring-resolve 'fdb.core/watch-config-path)]
+    (assoc m :fn :watch)))
 
 (defn sync [m]
-  (assoc m :fn :update))
+  (assoc m :fn :sync))
 
 (defn call [m]
   (assoc m :fn :call))
+
+(defn repl [m]
+  (assoc m :fn :repl))
 
 (defn help [m]
   (assoc m :fn :help))
 
 (def table
-  [{:cmds ["watch"]  :fn watch   :args->opts [:file]}
-   {:cmds ["sync"]   :fn sync    :args->opts [:file]}
-   {:cmds ["call"]   :fn call    :args->opts [:file]}
+  [{:cmds ["watch"]  :fn watch}
+   {:cmds ["sync"]   :fn sync}
+   {:cmds ["call"]   :fn call}
+   {:cmds ["repl"]   :fn repl}
    {:cmds []         :fn help}])
 
-(defn main [& args]
-  ;; docs https://github.com/babashka/cli#babashka-tasks
-  (println (cli/dispatch table args {:coerce {:depth :long}})))
+(def spec {:config {:desc    "The FileDB config file."
+                    :alias   :c
+                    :default "fdbconfig.edn"}})
 
+(defn -main [& args]
+  (println (cli/dispatch table args {:spec spec})))
