d20ac8b91841f3df509af88595989d2220d0a40a
Filipe Silva <filipematossilva@gmail.com>
2023-12-10 01:49:35 +0000

    realize the schedule lazy seq
    Otherwise the chime side effects never start

--- src/fdb/reactive.clj
+++ src/fdb/reactive.clj
@@ -118,18 +118,21 @@
                (do
                  (log/info "adding schedules for" id)
                  (assoc-in schedules [config-path id]
-                           (map-indexed
-                            (fn [trigger-idx {:keys [cron every] :as trigger}]
-                              (when-some [time-seq (u/catch-log
-                                                    (cond
-                                                      cron  (cron/times cron)
-                                                      every (chime/periodic-seq
-                                                             (t/now) (-> every u/duration-ms t/of-millis))))]
-                                (chime/chime-at time-seq
-                                                (fn [timestamp]
-                                                  (call doc :fdb.on/schedule trigger trigger-idx
-                                                        call-arg {:timestamp (str timestamp)})))))
-                            on-schedule)))
+                           (doall
+                            (map-indexed
+                             (fn [trigger-idx {:keys [cron every] :as trigger}]
+                               (when-some [time-seq (u/catch-log
+                                                     (cond
+                                                       cron  (cron/times cron)
+                                                       every (chime/periodic-seq
+                                                              (t/now) (-> every u/duration-ms t/of-millis))))]
+                                 (chime/chime-at time-seq
+                                                 (fn [timestamp]
+                                                   (call doc :fdb.on/schedule trigger trigger-idx
+                                                         call-arg {:timestamp (str timestamp)})
+                                                   ;; Never cancel schedule from fn.
+                                                   true))))
+                             on-schedule))))
                ;; There's no schedules for this doc, nothing to do.
                schedules)))))
 
